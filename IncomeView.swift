55e553ab251fc19a17e3b19735299ecca5ae814a
Solena <123829734+spop8@users.noreply.github.com>
2026-03-02 08:47:10 -0600

    Create IncomeView.swift

---
+++ IncomeView.swift
@@ -0,0 +1,35 @@
+//
+//  IncomeView.swift
+//  F.L.Y
+//
+//  Created by Solena Ornelas Pagnucci on 2/19/25.
+//
+
+import SwiftUI
+
+struct IncomeView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 40)
+                    .fill(Color(red: 0.561, green: 0.843, blue: 0.987))
+                    .frame(width: 500, height: 100)
+                Text("Income")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(hue: 0.587, saturation: 1.0, brightness: 0.817))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 40.0)
+                    .padding(.bottom, 0.0)
+            } 
+            Spacer()
+            RoundedRectangle(cornerRadius: 40)
+                .fill(Color(red: 0.561, green: 0.843, blue: 0.987))
+                .frame(width: 500, height: 100)
+        }
+        .ignoresSafeArea()    }
+}
+
+#Preview {
+    IncomeView()
+}
